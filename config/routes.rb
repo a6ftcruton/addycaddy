428c39e62813a93cdb3b996065305b8e432ab427
Jim Sutton <jimsuttonjimsutton@gmail.com>
2014-11-18 12:48:30 -0700

    move sign-in logic to navbar, add name to user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, :controllers => { omniauth_callbacks: "omniauth_callbacks" }
   get '/login', to: 'sessions#new'
   root to: 'sessions#new'
 end
