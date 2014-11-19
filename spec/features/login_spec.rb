d7ef1c65ad1ddbd5dc06cf5b3eb4fb30e04e0c0d
Jim Sutton <jimsuttonjimsutton@gmail.com>
2014-11-18 18:26:45 -0700

    Add factories for users, crimes, addresses

---
+++ spec/features/login_spec.rb
@@ -0,0 +1,12 @@
+require 'capybara/poltergeist'
+
+describe 'login process' do
+ 
+  it 'authenticates a user with twitter'
+  
+  context 'successful login' do
+  end
+
+  context 'unsuccessful login' do
+  end
+end
