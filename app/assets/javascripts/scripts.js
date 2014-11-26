1acdb9fc7c6ba0eee571134e0c71138f648a217d
Jim Sutton <jimsuttonjimsutton@gmail.com>
2014-11-26 15:24:01 -0700

    Update category-button js

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
   fadeFlashNotices(); 
- 
+  categoryClick(); 
+
   function fadeFlashNotices() {
     $('.alert-box').fadeIn(800).delay(3000).fadeOut(800);
+  }
+
+  function categoryClick() {
+   $('.category-button').click(function(e) {
+     var clicked = $(e.currentTarget).attr('class').split(/\s+/).pop();
+     $('.switch.radius').toggle("slow").not('.' + clicked + '-group').delay(200).hide();
+   }); 
   }
 });
