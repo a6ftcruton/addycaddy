61f76f8096a37126cedc2616bbe4780c554756ce
Jim Sutton <jimsuttonjimsutton@gmail.com>
2014-12-03 21:45:14 -0700

    change env syntax

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -4,8 +4,8 @@ if Rails.env.production?
       :port => 25,
       :domain => "http://192.241.133.37",
       :authentication => :plain,
-      :user_name => SENDGRID_USERNAME,
-      :password => SENDGRID_PASSWORD
+      :user_name => ENV['SENDGRID_USERNAME'],
+      :password => ENV['SENDGRID_PASSWORD']
  }
 end
   
