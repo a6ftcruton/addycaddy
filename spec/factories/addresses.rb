f16b8ecab4b041675341dc6aa842f745bf7619cd
Jim Sutton <jimsuttonjimsutton@gmail.com>
2015-02-09 15:35:02 -0700

    Remove zip= from factory, comparisons tests all green

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -1,13 +1,13 @@
 FactoryGirl.define do
   factory :address do
     address "1550 Blake St Denver, CO 80206"
-    zip 1111
+#    zip 1111
     
     initialize_with { new(address) }
   end
 
-  factory :address_in_denver, class: 'Address' do
-    address "1550 Blake St Denver, CO 80206"
-  end
+#  factory :address_in_denver, class: 'Address' do
+#    address "1550 Blake St Denver, CO 80206"
+#  end
 
 end
