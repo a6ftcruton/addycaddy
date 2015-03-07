aba7cbc1c6a6089f632bf6dc74f1ddf9e9668b34
Jim Sutton <jimsuttonjimsutton@gmail.com>
2015-03-07 10:50:53 -0700

    no longer relying on fetch_street_view method

--- lib/google_maps.rb
+++ lib/google_maps.rb
@@ -3,10 +3,9 @@ module GoogleMaps
   format :json
 
   base_uri 'https://maps.googleapis.com/maps/api'
-  #FIX
   default_params key: "AIzaSyAvMFhD3Lm5BiylwzPgboV0vZFT0AwxmEY"
 
-  def self.fetch_street_view(location)
-    get("/streetview", query: {size: "500x200", location: location })
-  end
+#  def self.fetch_street_view(location)
+#    get("/streetview", query: {size: "500x200", location: location })
+#  end
 end
